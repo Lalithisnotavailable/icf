d14dd5cb3552c55004502c30b8f534445bb64223
Lalithisnotavailable <109581867+Lalithisnotavailable@users.noreply.github.com>
2023-12-09 04:21:17 +0530

    Update new.php

--- new.php
+++ new.php
@@ -28,52 +28,5 @@
         "image": "https:\/\/icf.gov.in\/quiz\/images\/002.jpg"
     }
 [
-    {
-        "id": "1",
-        "question": "Which one is personal safety?|à¤•à¥Œà¤¨ à¤¸à¤¾ à¤µà¥à¤¯à¤•à¥à¤¤à¤¿à¤—à¤¤ à¤¸à¥à¤°à¤•à¥à¤·à¤¾ à¤¹à¥ˆ?",
-        "option1": "Keep the machine clean|à¤®à¤¶à¥€à¤¨ à¤•à¥‹ à¤¸à¤¾à¤« à¤°à¤–à¥‡à¤‚",
-        "option2": "Concentrate on your work|à¤…à¤ªà¤¨à¥‡ à¤•à¤¾à¤® à¤ªà¤° à¤§à¥à¤¯à¤¾à¤¨ à¤•à¥‡à¤‚à¤¦à¥à¤°à¤¿à¤¤ à¤•à¤°à¥‡à¤‚",
-        "option3": "Keep the gangway and floor clean|à¤—à¥ˆà¤‚à¤—à¤µà¥‡ à¤”à¤° à¤«à¥à¤²à¥‹à¤° à¤•à¥‹ à¤¸à¤¾à¤« à¤°à¤–à¥‡à¤‚",
-        "option4": "Keep the tools at their proper place|à¤‰à¤ªà¤•à¤°à¤£à¥‹à¤‚ à¤•à¥‹ à¤‰à¤¨à¤•à¥‡ à¤‰à¤šà¤¿à¤¤ à¤¸à¥à¤¥à¤¾à¤¨ à¤ªà¤° à¤°à¤–à¥‡à¤‚",
-        "correct_option": "B",
-        "trade": "Fitter",
-        "year": "First Year",
-        "subject": "Trade Theory",
-        "chapter": "testtt",
-        "image": 0
-    },
-    {
-        "id": "2",
-        "question": "Name the mandatory symbol.|à¤…à¤¨à¤¿à¤µà¤¾à¤°à¥à¤¯ à¤ªà¥à¤°à¤¤à¥€à¤• à¤•à¤¾ à¤¨à¤¾à¤® à¤¬à¤¤à¤¾à¤à¤‚à¥¤",
-        "option1": "Stop|à¤°à¥à¤•à¥‡à¤‚",
-        "option2": "Give way|à¤°à¤¾à¤¸à¥à¤¤à¤¾ à¤¦à¥‡à¤‚",
-        "option3": "Guarded|à¤¸à¤‚à¤°à¤•à¥à¤·à¤¿à¤¤",
-        "option4": "Unguarded|à¤…à¤¸à¤‚à¤°à¤•à¥à¤·à¤¿à¤¤",
-        "correct_option": "A",
-        "trade": "Fitter",
-        "year": "First Year",
-        "subject": "Trade Theory",
-        "chapter": "Module I",
-        "image": "https:\/\/icf.gov.in\/quiz\/images\/002.jpg"
-    } 
-]
-
-[
-    {
-        "id": "1",
-        "question": "Which one is personal safety?|à¤•à¥Œà¤¨ à¤¸à¤¾ à¤µà¥à¤¯à¤•à¥à¤¤à¤¿à¤—à¤¤ à¤¸à¥à¤°à¤•à¥à¤·à¤¾ à¤¹à¥ˆ?",
-        "option1": "Keep the machine clean|à¤®à¤¶à¥€à¤¨ à¤•à¥‹ à¤¸à¤¾à¤« à¤°à¤–à¥‡à¤‚",
-        "option2": "Concentrate on your work|à¤…à¤ªà¤¨à¥‡ à¤•à¤¾à¤® à¤ªà¤° à¤§à¥à¤¯à¤¾à¤¨ à¤•à¥‡à¤‚à¤¦à¥à¤°à¤¿à¤¤ à¤•à¤°à¥‡à¤‚",
-        "option3": "Keep the gangway and floor clean|à¤—à¥ˆà¤‚à¤—à¤µà¥‡ à¤”à¤° à¤«à¥à¤²à¥‹à¤° à¤•à¥‹ à¤¸à¤¾à¤« à¤°à¤–à¥‡à¤‚",
-        "option4": "Keep the tools at their proper place|à¤‰à¤ªà¤•à¤°à¤£à¥‹à¤‚ à¤•à¥‹ à¤‰à¤¨à¤•à¥‡ à¤‰à¤šà¤¿à¤¤ à¤¸à¥à¤¥à¤¾à¤¨ à¤ªà¤° à¤°à¤–à¥‡à¤‚",
-        "correct_option": "B",
-        "trade": "Plumber",
-        "year": "First Year",
-        "subject": "Trade Theory",
-        "chapter": "Plumber",
-        "image": 0
-    }
-]
-
-]
+   
 
