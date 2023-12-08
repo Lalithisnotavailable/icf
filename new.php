082527a9766ee8e4145d91a816d2155a4d6302cb
Lalithisnotavailable <109581867+Lalithisnotavailable@users.noreply.github.com>
2023-12-09 04:25:16 +0530

    Update new.php

--- new.php
+++ new.php
@@ -26,5 +26,19 @@
         "subject": "Trade Theory",
         "chapter": "Module I",
         "image": "https:\/\/icf.gov.in\/quiz\/images\/002.jpg"
+    },
+    {
+        "id": "1",
+        "question": "Which one is personal safety?|à¤•à¥Œà¤¨ à¤¸à¤¾ à¤µà¥à¤¯à¤•à¥à¤¤à¤¿à¤—à¤¤ à¤¸à¥à¤°à¤•à¥à¤·à¤¾ à¤¹à¥ˆ?",
+        "option1": "Keep the machine clean|à¤®à¤¶à¥€à¤¨ à¤•à¥‹ à¤¸à¤¾à¤« à¤°à¤–à¥‡à¤‚",
+        "option2": "Concentrate on your work|à¤…à¤ªà¤¨à¥‡ à¤•à¤¾à¤® à¤ªà¤° à¤§à¥à¤¯à¤¾à¤¨ à¤•à¥‡à¤‚à¤¦à¥à¤°à¤¿à¤¤ à¤•à¤°à¥‡à¤‚",
+        "option3": "Keep the gangway and floor clean|à¤—à¥ˆà¤‚à¤—à¤µà¥‡ à¤”à¤° à¤«à¥à¤²à¥‹à¤° à¤•à¥‹ à¤¸à¤¾à¤« à¤°à¤–à¥‡à¤‚",
+        "option4": "Keep the tools at their proper place|à¤‰à¤ªà¤•à¤°à¤£à¥‹à¤‚ à¤•à¥‹ à¤‰à¤¨à¤•à¥‡ à¤‰à¤šà¤¿à¤¤ à¤¸à¥à¤¥à¤¾à¤¨ à¤ªà¤° à¤°à¤–à¥‡à¤‚",
+        "correct_option": "B",
+        "trade": "Plumber",
+        "year": "First Year",
+        "subject": "Trade Theory",
+        "chapter": "plumbs",
+        "image": 0
     }
 ]
