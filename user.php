1463c2307e5b97ea9b6cf5add0a1a311008a6d90
Lalithisnotavailable <109581867+Lalithisnotavailable@users.noreply.github.com>
2023-12-13 12:46:38 +0530

    Create user.php

---
+++ user.php
@@ -0,0 +1,14 @@
+[
+    {
+        "id": "1",
+        "username": "lalith",
+        "password":"12345"
+
+    },
+    {
+        "id": "2",
+        "username": "lals",
+        "password":"1111"
+
+    }
+]
